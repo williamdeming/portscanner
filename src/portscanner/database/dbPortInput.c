159eda5301281fdd5599faefbdcbca250eb4213e
williamdeming <wm.deming@gmail.com>
2018-04-01 23:21:01 -0400

    Fix missing parentheses in database scripts

--- src/portscanner/database/dbPortInput.c
+++ src/portscanner/database/dbPortInput.c
@@ -27,9 +27,9 @@ void dbPortInput(int id, int port, int status, int expected_status, char *host, 
 
   mysql_query(con, "USE portscan");
 
-  mysql_query(con, "CREATE TABLE IF NOT EXISTS computers(id INT unsigned AUTO_INCREMENT, ip INT(15), network VARCHAR(20), PRIMARY KEY (id)");
+  mysql_query(con, "CREATE TABLE IF NOT EXISTS computers(id INT unsigned AUTO_INCREMENT, ip INT(15), network VARCHAR(20), PRIMARY KEY (id))");
 
-  mysql_query(con, "CREATE TABLE IF NOT EXISTS ports(id INT(20), port INT(7), status INT(1), expected_status INT(1)");
+  mysql_query(con, "CREATE TABLE IF NOT EXISTS ports(id INT(20), port INT(7), status INT(1), expected_status INT(1))");
 
   sprintf(buffer, "INSERT INTO ports(%d, %d, %d, %d)", id, port, status, expected_status);
   mysql_query(con, buffer);
